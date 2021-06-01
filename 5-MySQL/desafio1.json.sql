1230abe696f460a59c84c71b1ec8779933dbb86d
Rafael Greco <rafaelmgreco@hotmail.com>
2021-06-01 13:51:18 -0300

    adicionando projetos

---
+++ 5-MySQL/desafio1.json.sql
@@ -0,0 +1,16 @@
+{
+  "coluna_usuario": "Nome",
+  "tabela_que_contem_usuario": "Usuarios",
+  "coluna_plano": "planoNome",
+  "tabela_que_contem_plano": "Planos",
+  "coluna_historico_de_reproducoes": "musica_id",
+  "tabela_que_contem_historico_de_reproducoes": "Historico",
+  "coluna_seguindo_artistas": "artista_id",
+  "tabela_que_contem_seguindo_artistas": "Seguidores",
+  "coluna_artista": "artistaNome",
+  "tabela_que_contem_artista": "Artistas",
+  "coluna_album": "album",
+  "tabela_que_contem_album": "Albums",
+  "coluna_cancoes": "musicaNome",
+  "tabela_que_contem_cancoes": "Musicas"
+}
